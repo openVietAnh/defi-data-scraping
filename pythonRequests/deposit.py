c2dcd7b81b384d242c18002aa4b0025a88a9d497
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-05-19 21:09:23 +0700

    get all time deposit transactions in V2

---
+++ pythonRequests/deposit.py
@@ -0,0 +1,58 @@
+import requests
+import csv
+from datetime import datetime
+
+keys, transactions = None, []
+current_time = int(datetime.now().timestamp())
+
+while True:
+    query = """
+    {
+        deposits(where: {timestamp_lt: """ + str(current_time) + """}, first: 1000, orderBy: timestamp, orderDirection: desc) {
+            user {
+                id
+            }
+            caller {
+                id
+            }
+            reserve {
+                symbol
+            }
+            amount
+            timestamp
+        }
+    }
+    """
+    response = requests.post('https://api.thegraph.com/subgraphs/name/aave/protocol-v2'
+                                '',
+                                json={'query': query})
+    if response.status_code == 200:
+        print("Read from timestamp", current_time, "successfully:", response.status_code)
+    else:
+        print("Problem reading from timestamp", current_time, ":", response.status_code)
+    
+    try:
+        data = response.json()["data"]["deposits"]
+    except Exception:
+        print("Error at timestamp", current_time)
+
+    print(len(data), "transactions found")
+    if len(data) == 0:
+        break
+    
+    if keys is None:
+        keys = data[0].keys()
+
+    for transaction in data:
+        transaction["user"] = transaction["user"]["id"]
+        transaction["reserve"] = transaction["reserve"]["symbol"]
+        transaction["caller"] = transaction["caller"]["id"]
+        transactions.append(transaction)
+
+    current_time = int(data[-1]["timestamp"])
+
+with open('deposit.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(transactions)
+        
