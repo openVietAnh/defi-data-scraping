3d4755853b4a16eceeb9917eef03a3818b2fb307
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-05-20 21:46:17 +0700

    get all redeem underlying transactions in AAVE V2

---
+++ pythonRequests/redeemUnderlying.py
@@ -0,0 +1,63 @@
+import requests
+import csv
+from datetime import datetime
+
+keys, transactions = None, []
+current_time = int(datetime.now().timestamp())
+
+while True:
+    query = """
+    {
+        redeemUnderlyings(where: {timestamp_lt:""" + str(current_time) + """}, first: 1000, orderBy: timestamp, orderDirection: desc) {
+            id
+            pool {
+                id
+            }
+            user {
+                id
+            }
+            to {
+                id
+            }
+            reserve {
+                symbol
+            }
+            amount
+            timestamp
+        }
+    }
+    """
+    response = requests.post('https://api.thegraph.com/subgraphs/name/aave/protocol-v2'
+                                '',
+                                json={'query': query})
+    if response.status_code == 200:
+        print("Read from timestamp", current_time, "successfully:", response.status_code)
+    else:
+        print("Problem reading from timestamp", current_time, ":", response.status_code)
+    
+    try:
+        data = response.json()["data"]["redeemUnderlyings"]
+    except Exception:
+        print("Error at timestamp", current_time)
+
+    print(len(data), "transactions found")
+    if len(data) == 0:
+        break
+    
+    if keys is None:
+        keys = data[0].keys()
+
+    for transaction in data:
+        transaction["user"] = transaction["user"]["id"]
+        transaction["to"] = transaction["to"]["id"]
+        transaction["reserve"] = transaction["reserve"]["symbol"]
+        transaction["pool"] = transaction["pool"]["id"]
+        transactions.append(transaction)
+
+    current_time = int(data[-1]["timestamp"])
+
+with open('redeemUnderlying.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(transactions)
+        
