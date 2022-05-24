9929ec33da4b7477391d7e8b5bb1a2e666023cfa
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-05-24 21:58:09 +0700

    fix missing transactions bug

--- pythonRequests/allTransaction.py
+++ pythonRequests/allTransaction.py
@@ -3,12 +3,14 @@ import csv
 from datetime import datetime
 
 keys, transactions = None, []
+error = []
 current_time = int(datetime.now().timestamp())
+last_transactions = set()
 
 while True:
     query = """
     {
-        userTransactions(where: {timestamp_lt: """ + str(current_time) + """}, first: 1000, orderBy: timestamp, orderDirection: desc) {
+        userTransactions(where: {timestamp_lte: """ + str(current_time) + """}, first: 1000, orderBy: timestamp, orderDirection: desc) {
             id
             pool {
                 id
@@ -23,29 +25,42 @@ while True:
     response = requests.post('https://api.thegraph.com/subgraphs/name/aave/protocol-v2'
                                 '',
                                 json={'query': query})
-    if response.status_code == 200:
-        print("Read from timestamp", current_time, "successfully:", response.status_code)
-    else:
+    if response.status_code != 200:
         print("Problem reading from timestamp", current_time, ":", response.status_code)
+        continue
     
     try:
         data = response.json()["data"]["userTransactions"]
     except Exception:
         print("Error at timestamp", current_time)
+        continue
 
-    print(len(data), "transactions found")
     if len(data) == 0:
         break
     
     if keys is None:
         keys = data[0].keys()
 
-    for transaction in data:
+    index = 0
+    try:
+        while data[index]["id"] in last_transactions:
+            index += 1
+    except IndexError:
+            current_time -= 1
+            continue
+    print(len(data) - index, "transactions found at timestamp", current_time)
+
+    for transaction in data[index:]:
         transaction["pool"] = transaction["pool"]["id"]
         transaction["user"] = transaction["user"]["id"]
         transactions.append(transaction)
 
     current_time = int(data[-1]["timestamp"])
+    index = -1
+    last_transactions = set()
+    while data[index]["timestamp"] == data[-1]["timestamp"]:
+        last_transactions.add(data[index]["id"])
+        index -= 1
 
 with open('allTransaction.csv', 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
--- pythonRequests/rebalanceStableBorrowRate.py
+++ pythonRequests/rebalanceStableBorrowRate.py
@@ -4,11 +4,12 @@ from datetime import datetime
 
 keys, transactions = None, []
 current_time = int(datetime.now().timestamp())
+last_transactions = set()
 
 while True:
     query = """
     {
-        rebalanceStableBorrowRates(where: {timestamp_lt: """ + str(current_time) + """}, first: 1000, orderBy: timestamp, orderDirection: desc) {
+        rebalanceStableBorrowRates(where: {timestamp_lte: """ + str(current_time) + """}, first: 1000, orderBy: timestamp, orderDirection: desc) {
             id
             pool {
                 id
@@ -28,30 +29,43 @@ while True:
     response = requests.post('https://api.thegraph.com/subgraphs/name/aave/protocol-v2'
                                 '',
                                 json={'query': query})
-    if response.status_code == 200:
-        print("Read from timestamp", current_time, "successfully:", response.status_code)
-    else:
+    if response.status_code != 200:
         print("Problem reading from timestamp", current_time, ":", response.status_code)
+        continue
     
     try:
         data = response.json()["data"]["rebalanceStableBorrowRates"]
     except Exception:
         print("Error at timestamp", current_time)
+        continue
 
-    print(len(data), "transactions found")
     if len(data) == 0:
         break
     
     if keys is None:
         keys = data[0].keys()
 
-    for transaction in data:
+    index = 0
+    try:
+        while data[index]["id"] in last_transactions:
+            index += 1
+    except IndexError:
+            current_time -= 1
+            continue
+    print(len(data) - index, "transactions found at timestamp", current_time)
+
+    for transaction in data[index:]:
         transaction["user"] = transaction["user"]["id"]
         transaction["pool"] = transaction["pool"]["id"]
         transaction["reserve"] = transaction["reserve"]["symbol"]
         transactions.append(transaction)
 
     current_time = int(data[-1]["timestamp"])
+    index = -1
+    last_transactions = set()
+    while data[index]["timestamp"] == data[-1]["timestamp"]:
+        last_transactions.add(data[index]["id"])
+        index -= 1
 
 with open('rebalanceStableBorrowRate.csv', 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
