c52423e787916481defc373c6aec01a8e060b89c
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-03 13:51:14 +0700

    categorize transactions by token reserve

---
+++ csvData/filter_token_transaction.py
@@ -0,0 +1,39 @@
+import csv
+
+token_lst = ["DAI", "WBTC", "WETH", "USDC", "USDT"]
+hash_to_token = {}
+
+token_transactions = {token: [] for token in token_lst}
+keys = ["id", "type", "user", "pool", "timestamp"]
+
+file_config = [
+    ("borrow", 3),
+    ("deposit", 3),
+    ("repay", 4),
+    ("usageAsCollateral", 3),
+    ("redeemUnderlying", 4),
+    ("swap", 3),
+    ("liquidationCall", 3),
+    ("rebalanceStableBorrowRate", 3),
+    ("flashLoan", 1),
+]
+
+for file in file_config:
+    with open("../csvData/" + file[0] + ".csv", "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        next(reader, None)
+        for item in reader:
+            hash_to_token[item[0].split(":")[2]] = item[file[1]]
+
+with open("../csvData/allTransactionType.csv", "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    next(reader, None)
+    for item in reader:
+        if hash_to_token[item[0].split(":")[2]] in token_lst:
+            token_transactions[hash_to_token[item[0].split(":")[2]]].append({keys[index]: item[index] for index in range(len(keys))})
+
+for token in token_lst:
+    with open("./tokenTransactions/" + token + ".csv", 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(token_transactions[token])
