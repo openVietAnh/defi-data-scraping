7ce1ed7c1064b0886228797544d8d6765f311ff9
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-09 20:42:49 +0700

    update more USD's ETH price data

--- pythonRequests/getUSD-ETHPrice.py
+++ pythonRequests/getUSD-ETHPrice.py
@@ -1,36 +1,47 @@
-from http.client import responses
 import requests
 import csv
+from datetime import datetime
 
-keys = None
-token_lst = ["USDT", "USDC", "DAI", "WBTC", "WETH"]
+keys, prices = ["timestamp", "price"], []
+current_time = 1654016400
 
-for index, token in enumerate(token_lst):
-    timestamps = []
-    with open("../csvData/" + token + "-price.csv") as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        next(reader, None)
-        for item in reader:
-            timestamps.append(int(item[1])) 
+while True:
     query = """
     {
-        usdEthPriceHistoryItems(where: {timestamp_in: """ + str(timestamps) + """}, orderBy: timestamp, orderDirection: desc) {
-            price
+        usdEthPriceHistoryItems(
+            first: 1000, 
+            where: {timestamp_lt: """ + str(current_time) + """},
+            orderBy: timestamp,
+            orderDirection: desc
+        ) {
             timestamp
+            price
         }
     }
     """
     response = requests.post('https://api.thegraph.com/subgraphs/name/aave/protocol-v2'
                                 '',
                                 json={'query': query})
-    # print(response.status_code)
-    # print(response.json())
-    data = response.json()["data"]["usdEthPriceHistoryItems"]
-    
-    if keys is None:
-        keys = data[0].keys()
+    if response.status_code != 200:
+        print("Problem reading from timestamp", current_time, ":", response.status_code)
+        continue
+    try:
+        data = response.json()["data"]["usdEthPriceHistoryItems"]
+    except Exception:
+        print("Error at timestamp", current_time)
+        continue
 
-    with open("../usdETHprice/" + str(index) + '.csv', 'w', newline='') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(data)
+    if len(data) == 0:
+        break
+
+    print(len(data), "rows found at timestamp", current_time)
+
+    prices += data
+
+    current_time = int(data[-1]["timestamp"])
+
+with open('../usdETHprice/full.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(prices)
+        
