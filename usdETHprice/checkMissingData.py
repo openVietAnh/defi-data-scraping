2f7a01ac060c63a1279d3d310ac6a848cc7b383f
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-13 20:16:17 +0700

    check for missing timestamps

--- usdETHprice/checkMissingData.py
+++ usdETHprice/checkMissingData.py
@@ -3,18 +3,18 @@ import csv
 token_lst = ["WBTC", "DAI", "USDT", "USDC"]
 missing = []
 timestamp_to_price = {}
-with open("fullPrices.csv") as csvfile:
+with open("full_updated.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     next(reader, None)
     for item in reader:
         timestamp_to_price[item[0]] = item[1]
 
 for token in token_lst:
-    with open("../csvData/" + token + "-price.csv") as csvfile:
+    with open("../csvData/reserveInfo/" + token + "_info.csv") as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         next(reader, None)
         for item in reader:
-            if item[1] not in timestamp_to_price.keys():
-                missing.append(item[1])
+            if item[0] not in timestamp_to_price.keys():
+                missing.append(item[0])
 
-print(missing)
+print(len(missing))
