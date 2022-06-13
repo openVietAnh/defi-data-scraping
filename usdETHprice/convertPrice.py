e419161764d0d19c515d2eef058ac0cef1e75751
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-13 21:12:40 +0700

    convert token price from ETH to USD

--- usdETHprice/convertPrice.py
+++ usdETHprice/convertPrice.py
@@ -2,22 +2,29 @@ import csv
 
 token_lst = ["WBTC", "DAI", "USDT", "USDC"]
 timestamp_to_usd_price = {}
-with open("fullPrices.csv") as csvfile:
+with open("full_updated.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     next(reader, None)
     for item in reader:
         timestamp_to_usd_price[item[0]] = item[1]
 
 for token in token_lst:
+    print(token)
     data = []
-    with open("../csvData/" + token + "-price.csv") as csvfile:
+    with open("../csvData/tokenPriceinETH/" + token + "_updated.csv") as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         next(reader, None)
         for item in reader:
-            eth_price = int(item[0])
-            usd_price = eth_price / float(timestamp_to_usd_price[item[1]])
-            data.append({"timestamp": item[1], "price": usd_price})
-    with open("../csvData/" + token + "-usd-price.csv", 'w', newline='') as output_file:
+            if token == "WBTC" and len(item[1]) == 15:
+                eth_price = int(item[1]) * 100000
+            else:
+                eth_price = int(item[1])
+            try:
+                usd_price = eth_price / float(timestamp_to_usd_price[item[0]])
+                data.append({"timestamp": item[0], "price": usd_price})
+            except KeyError:
+                pass
+    with open("../csvData/tokenPriceinUSD/" + token + "-usd-price.csv", 'w', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, ["timestamp", "price"])
         dict_writer.writeheader()
         dict_writer.writerows(data)
