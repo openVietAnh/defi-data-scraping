eea307e98eacaf904ba491ad2d3f70655d7df2ac
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-13 22:02:18 +0700

    optimize calculation

--- usdETHprice/calculateMissingPrices.py
+++ usdETHprice/calculateMissingPrices.py
@@ -16,12 +16,13 @@ with open("full.csv", "r") as csv_file:
         time_to_price[int(item[0])] = int(item[1])
 
 for token in token_lst:
+    start_index = 0
     with open("../csvData/reserveInfo/" + token + "_info.csv", "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         next(reader, None)
         for item in reader:
             if item[0] not in timestamps:
-                index, time = 0, int(item[0])
+                index, time = start_index, int(item[0])
                 if time > time_lst[0]:
                     data.append({"timestamp": item[0], "price": time_to_price[time_lst[0]]})
                 elif time < time_lst[-1]:
@@ -29,6 +30,7 @@ for token in token_lst:
                 else:
                     while not time_lst[index] > time > time_lst[index + 1]:
                         index += 1
+                    start_index = index
                     before_price = time_to_price[time_lst[index + 1]]
                     after_price = time_to_price[time_lst[index]]
                     price_diff = after_price - before_price
