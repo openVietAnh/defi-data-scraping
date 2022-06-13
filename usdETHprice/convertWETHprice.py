9e99b05e02bdbfcf0c788eadf5b2b9cba781a4af
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-13 21:12:40 +0700

    convert token price from ETH to USD

--- usdETHprice/convertWETHprice.py
+++ usdETHprice/convertWETHprice.py
@@ -1,13 +1,13 @@
 import csv
 
 data = []
-with open("fullPrices.csv") as csvfile:
+with open("full_updated.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     next(reader, None)
     for item in reader:
         data.append({"timestamp": item[0], "price": 1/ (float(item[1]) / 1000000000000000000)})
 
-with open("../csvData/WETH-usd-price.csv", 'w', newline='') as output_file:
+with open("../csvData/tokenPriceinUSD/WETH-usd-price.csv", 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, ["timestamp", "price"])
     dict_writer.writeheader()
     dict_writer.writerows(data)
