9fc9440f1427ce169b5cf0b423c1461c19ab4bb2
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-07-16 11:24:12 +0700

    extend user count data and fix time range

--- csvData/1-minute-mark/get_user_count.py
+++ csvData/1-minute-mark/get_user_count.py
@@ -1,25 +1,21 @@
 import csv
 import datetime
 
-# Start-time
-# DAI: 2020-12-01T23:40:50
-# USDC: 2020-12-02T20:07:35 (latest)
-# USDT: 2020-12-02T17:15:31
-# WBTC: 2020-12-02T19:55:51
-# WETH: 2020-12-01T06:11:40
-
-# End-time
-# DAI: 2022-05-31T23:04:51 (earliest)
+# DAI: 1606840850
+# USDC: 1606914455 (latest)
+# USDT: 1606904131
+# WBTC: 1606913751
+# WETH: 1606777900
 
 token_lst = ["USDC", "USDT", "WBTC", "WETH", "DAI"]
-keys = ["timestamp", "time", "blockNumber", "totalDeposits", "depositRate", "stableBorrowRate", "variableBorrowRate", "utilizationRate"]
+keys = ["timestamp", "userCount"]
 start_time = datetime.datetime(2020, 12, 2, 20, 8, 0)
 end_time = datetime.datetime(2022, 5, 31, 23, 5, 0)
 
 for token in token_lst:
     data = []
     time = start_time
-    with open("../reserveInfo/" + token + "_TLV_USD.csv", "r") as csvfile:
+    with open("../tokenUserCount/" + token + ".csv", "r") as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         next(reader, None)
         for item in reader:
@@ -30,17 +26,15 @@ for token in token_lst:
                 continue
             while time <= row_time and time <= end_time:
                 info["timestamp"] = int(time.timestamp())
-                info["time"] = time.isoformat(timespec='seconds')
                 data.append(dict(info))
                 time += datetime.timedelta(minutes=1)
             info = new_info
             if row_time > end_time:
                 break
         info["timestamp"] = int(time.timestamp())
-        info["time"] = time.isoformat(timespec='seconds')
         data.append(dict(info))
 
-        with open(token + ".csv", 'w', newline='') as output_file:
+        with open(token + "-user.csv", 'w', newline='') as output_file:
             dict_writer = csv.DictWriter(output_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(data)
