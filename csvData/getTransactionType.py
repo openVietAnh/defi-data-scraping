04066447d42e7e0e49636f88e9f317e01e8304a5
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-08-01 22:34:10 +0700

    update new blocks

--- csvData/getTransactionType.py
+++ csvData/getTransactionType.py
@@ -1,10 +1,10 @@
 import csv
 
-TYPE = ("borrow", "deposit", "flashLoan", "liquidationCall", "rebalanceStableBorrowRate", "redeemUnderlying", "repay", "reserves", "swap", "usageAsCollateral")
+TYPE = ("borrow", "deposit", "flashLoan", "liquidationCall", "redeemUnderlying", "repay", "swap", "usageAsCollateral")
 transactionType = {}
 
 for file_name in TYPE:
-    with open(file_name + ".csv", "r") as csv_file:
+    with open("./update_data/" + file_name + "_update.csv", "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         next(reader, None)
         for item in reader:
@@ -13,7 +13,7 @@ for file_name in TYPE:
 data, keys = [], ["id", "pool", "user", "timestamp"]
 typeNotFoundData = []
 count = 0
-with open("allTransaction.csv", "r") as csv_file:
+with open("./update_data/allTransaction_update.csv", "r") as csv_file:
     reader = csv.reader(csv_file, delimiter=",")
     next(reader, None)
     for item in reader:
@@ -25,11 +25,11 @@ with open("allTransaction.csv", "r") as csv_file:
             typeNotFoundData.append(dct)
             count += 1
 print("Missing type:", count)
-with open('allTransactionType.csv', 'w', newline='') as output_file:
+with open('./update_data/allTransactionType.csv', 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, ["id", "type", "user", "pool", "timestamp"] )
     dict_writer.writeheader()
     dict_writer.writerows(data)
-with open('missing.csv', 'w', newline='') as output_file:
+with open('./update_data/missing.csv', 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, ["id", "user", "pool", "timestamp"] )
     dict_writer.writeheader()
     dict_writer.writerows(typeNotFoundData)
