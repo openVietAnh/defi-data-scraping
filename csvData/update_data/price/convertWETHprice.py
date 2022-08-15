a69a48385194f7de20cf0fe16cc7758fd8b81e3f
Viet Anh Tran <42197149+vietanhtran2710@users.noreply.github.com>
2022-08-15 11:17:15 +0700

    calculate token price in USD

---
+++ csvData/update_data/price/convertWETHprice.py
@@ -0,0 +1,13 @@
+import csv
+
+data = []
+with open("usd_ethUpdatedPrice.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    next(reader, None)
+    for item in reader:
+        data.append({"timestamp": item[0], "price": 1/ (float(item[1]) / 1000000000000000000)})
+
+with open("WETH-usd-price.csv", 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, ["timestamp", "price"])
+    dict_writer.writeheader()
+    dict_writer.writerows(data)
