a7eaa29a29a0afec7a3246460441efabe58be539
Viet Anh Tran <42197149+vietanhtran2710@users.noreply.github.com>
2022-08-15 13:21:55 +0700

    sort WBTC transactions by timestamp

---
+++ csvData/tokenTransactions/sort.py
@@ -0,0 +1,20 @@
+import csv
+
+token_lst = ["DAI", "USDC", "USDT", "WBTC", "WETH"]
+keys = ["id", "type", "user", "pool", "timestamp"]
+
+for token in ["WBTC"]:
+    data = []
+
+    with open(token + ".csv", "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        next(reader, None)
+        for item in reader:
+            data.append({keys[i]: item[i] for i in range(len(keys))})
+
+    data.sort(key = lambda x: x["timestamp"])
+
+    with open(token + '.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
