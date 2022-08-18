20b2fc255d1d920f5aa69b84775724087fb68617
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-08-18 14:30:30 +0700

    update blocks needed to calculate HHI

--- csvData/userFunds/filter_token.py
+++ csvData/userFunds/filter_token.py
@@ -1,7 +1,7 @@
 import csv
-from web3 import Web3, EthereumTesterProvider
-w3 = Web3(Web3.HTTPProvider('https://eth-mainnet.alchemyapi.io/v2/0e3D_mlVqAhNuFvqu1-Exd3ElNON88KE'))
-print(w3.isConnected())
+# from web3 import Web3, EthereumTesterProvider
+# w3 = Web3(Web3.HTTPProvider('https://eth-mainnet.alchemyapi.io/v2/0e3D_mlVqAhNuFvqu1-Exd3ElNON88KE'))
+# print(w3.isConnected())
 
 # deposit
 # id,user,caller,reserve,amount,timestamp
@@ -19,23 +19,24 @@ reserve_index = {"deposit": 3, "liquidationCall": 3, "redeemUnderlying": 4}
 token_block = {token: [] for token in token_lst}
 
 for token in token_lst:
-    with open("../hashtoBlockNum/" + token + "_block.csv", "r") as f:
+    with open("../hashtoBlockNum/" + token + "_filtered.csv", "r") as f:
         reader = csv.reader(f, delimiter=",")
         next(reader, None)
         for item in reader:
             hash_to_block[item[0]] = item[1]
 
 for f in files:
+    count = 0
     with open("../" + f + ".csv", "r") as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         next(reader, None)
         for item in reader:
             if item[reserve_index[f]] in token_lst:
                 try:
-                    token_block[item[reserve_index[f]]].append(int(hash_to_block[item[0]]))
+                    token_block[item[reserve_index[f]]].append(int(hash_to_block[item[0].split(":")[2]]))
                 except KeyError:
-                    block_number = w3.eth.get_transaction_receipt(item[0].split(":")[2])["blockNumber"]
-                    token_block[item[reserve_index[f]]].append(int(block_number))
+                    count += 1
+    print(f, count)
 
 for token in token_lst:
     token_block[token].sort()
