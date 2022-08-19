2ab45c41c2526d6e127ec7fa94125ae86fed2b63
Viet Anh Tran <42197149+vietanhtran2710@users.noreply.github.com>
2022-08-19 16:08:53 +0700

    update missing prices

--- csvData/tokenPriceinETH/update_missing_prices.py
+++ csvData/tokenPriceinETH/update_missing_prices.py
@@ -8,7 +8,7 @@ for token in token_lst:
     time_lst = []
     time_to_price = {}
     data = []
-    with open(token + "_price_in_eth.csv", "r") as csv_file:
+    with open(token + "_updated.csv", "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         next(reader, None)
         for item in reader:
@@ -16,6 +16,8 @@ for token in token_lst:
             time_lst.append(int(item[0]))
             data.append({"timestamp": item[0], "price": item[1]})
             time_to_price[int(item[0])] = int(item[1])
+
+    start_index = 0
     
     with open("../reserveInfo/" + token + "_info.csv", "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
@@ -23,14 +25,15 @@ for token in token_lst:
         for item in reader:
             if item[0] not in timestamps:
                 # print(item[0])
-                index, time = 0, int(item[0])
-                if time > time_lst[0]:
+                index, time = start_index, int(item[0])
+                if time < time_lst[0]:
                     data.append({"timestamp": item[0], "price": time_to_price[time_lst[0]]})
-                elif time < time_lst[-1]:
+                elif time > time_lst[-1]:
                     data.append({"timestamp": item[0], "price": time_to_price[time_lst[-1]]})
                 else:
-                    while not time_lst[index] > time > time_lst[index + 1]:
+                    while not time_lst[index] < time < time_lst[index + 1]:
                         index += 1
+                    start_index = index
                     before_price = time_to_price[time_lst[index + 1]]
                     after_price = time_to_price[time_lst[index]]
                     price_diff = after_price - before_price
