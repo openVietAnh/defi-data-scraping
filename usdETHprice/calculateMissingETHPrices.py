87577a5600c3b94071c12f45f882c4047e6f2acb
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-10-11 22:33:42 +0700

    pylint clean code

--- usdETHprice/calculateMissingETHPrices.py
+++ usdETHprice/calculateMissingETHPrices.py
@@ -1,15 +1,18 @@
+# pylint: disable-msg=C0103
+"""
+    Calculate ETH price at multiple needed timestamps that AAVE V2 subgraph did not store
+"""
 import csv
 
-token_lst = ["DAI", "WBTC", "USDC", "USDT"]
 timestamps = set()
 time_lst = []
 time_to_price = {}
 data = []
 
 with open("../csvData/tokenPriceinUSD/WETH-usd-price.csv", "r") as csv_file:
-    reader = csv.reader(csv_file, delimiter=",")
-    next(reader, None)
-    for item in reader:
+    READER = csv.reader(csv_file, delimiter=",")
+    next(READER, None)
+    for item in READER:
         timestamps.add(item[0])
         time_lst.append(int(item[0]))
         data.append({"timestamp": item[0], "price": item[1]})
@@ -18,9 +21,9 @@ with open("../csvData/tokenPriceinUSD/WETH-usd-price.csv", "r") as csv_file:
 start_index = 0
 
 with open("../csvData/reserveInfo/WETH_info.csv", "r") as csv_file:
-    reader = csv.reader(csv_file, delimiter=",")
-    next(reader, None)
-    for item in reader:
+    READER = csv.reader(csv_file, delimiter=",")
+    next(READER, None)
+    for item in READER:
         if item[0] not in timestamps:
             index, time = start_index, int(item[0])
             if time < time_lst[0]:
@@ -35,12 +38,13 @@ with open("../csvData/reserveInfo/WETH_info.csv", "r") as csv_file:
                 after_price = time_to_price[time_lst[index]]
                 price_diff = after_price - before_price
                 time_diff = time_lst[index] - time_lst[index + 1]
-                calculated_price = before_price + (price_diff / time_diff) * (int(item[0]) - time_lst[index + 1])
+                change = (price_diff / time_diff) * (int(item[0]) - time_lst[index + 1])
+                calculated_price = before_price + change
                 data.append({"timestamp": item[0], "price": float(calculated_price)})
 
 data.sort(key=lambda x: x["timestamp"])
 
 with open('../csvData/tokenPriceinUSD/WETH-usd-price.csv', 'w', newline='') as output_file:
-    dict_writer = csv.DictWriter(output_file, ["timestamp", "price"])
-    dict_writer.writeheader()
-    dict_writer.writerows(data)
+    DICT_WRITER = csv.DictWriter(output_file, ["timestamp", "price"])
+    DICT_WRITER.writeheader()
+    DICT_WRITER.writerows(data)
--- usdETHprice/checkMissingData.py
+++ usdETHprice/checkMissingData.py
@@ -1,19 +1,24 @@
+# pylint: disable-msg=C0103
+"""
+    Check for missing needed price timestamp from reserve info file
+"""
 import csv
 
-token_lst = ["WBTC", "DAI", "USDT", "USDC"]
+
+TOKEN_LST = ["WBTC", "DAI", "USDT", "USDC"]
 missing = []
 timestamp_to_price = {}
 with open("full_updated.csv") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    next(reader, None)
-    for item in reader:
+    READER = csv.reader(csvfile, delimiter=",")
+    next(READER, None)
+    for item in READER:
         timestamp_to_price[item[0]] = item[1]
 
-for token in token_lst:
+for token in TOKEN_LST:
     with open("../csvData/reserveInfo/" + token + "_info.csv") as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
-        next(reader, None)
-        for item in reader:
+        READER = csv.reader(csvfile, delimiter=",")
+        next(READER, None)
+        for item in READER:
             if item[0] not in timestamp_to_price.keys():
                 missing.append(item[0])
 
