e1937df7088c6bf545303e9b24d5a44de28430db
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-10-18 20:30:51 +0700

    add WETH historical borrowers count

--- csvData/borrower/checkMissingData.py
+++ csvData/borrower/checkMissingData.py
@@ -1,12 +1,12 @@
 import csv
 s = set()
-with open("DAI_borrower.csv", "r") as f:
+with open("WETH_borrower.csv", "r") as f:
     reader = csv.reader(f, delimiter=",")
     next(reader, None)
     for item in reader:
         s.add(item[0])
 count = 0
-with open("DAI_borrower_block.csv", "r") as f:
+with open("WETH_borrower_block.csv", "r") as f:
     data = f.readlines()
     for item in data:
         if item.strip() not in s:
