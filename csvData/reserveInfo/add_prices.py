3c63f372196cf4e7e936d8202776987722f4a3ff
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-10-22 09:33:57 +0700

    add historical prices to reserves info file

---
+++ csvData/reserveInfo/add_prices.py
@@ -0,0 +1,22 @@
+import csv
+
+price = {}
+with open("../tokenPriceinUSD/DAI-usd-price.csv", "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    next(reader, None)
+    for item in reader:
+        price[item[0]] = float(item[1])
+
+data = []
+keys = ["timestamp","time","blockNumber","totalDeposits","depositRate","stableBorrowRate","variableBorrowRate","utilizationRate","userCount","HHI","depositers","borrowers","price"]
+with open("DAI_info.csv", "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    next(reader, None)
+    for item in reader:
+        row = {keys[i]: item[i] for i in range(len(keys) - 1)}
+        row["price"] = price[item[0]]
+        data.append(row)
+with open('DAI_full_info.csv', 'w', newline='') as output_file:
+        DICT_WRITER = csv.DictWriter(output_file, keys)
+        DICT_WRITER.writeheader()
+        DICT_WRITER.writerows(data)
---
+++ csvData/reserveInfo/check_missing_prices.py
@@ -0,0 +1,18 @@
+import csv
+
+price = {}
+with open("../tokenPriceinUSD/DAI-usd-price.csv", "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    next(reader, None)
+    for item in reader:
+        price[item[0]] = float(item[1])
+
+count = 0
+with open("DAI_info.csv", "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    next(reader, None)
+    for item in reader:
+        if item[0] not in price:
+            count += 1
+
+print(count)
