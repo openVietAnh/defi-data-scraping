1963e75081506ed3aea571b0f1d1fb4b668d7f9d
vietanhtran2710 <vietanhtran.uet@gmail.com>
2023-01-07 20:31:21 +0700

    add AAVE depositers/borrowers count

--- csvData/borrower/checkMissingData.py
+++ csvData/borrower/checkMissingData.py
@@ -1,14 +1,18 @@
 import csv
 s = set()
-with open("WETH_borrower.csv", "r") as f:
+with open("WBTC_borrowers.csv", "r") as f:
     reader = csv.reader(f, delimiter=",")
     next(reader, None)
     for item in reader:
         s.add(item[0])
 count = 0
-with open("WETH_borrower_block.csv", "r") as f:
+lines = []
+with open("WBTC_borrower_block.csv", "r") as f:
     data = f.readlines()
     for item in data:
         if item.strip() not in s:
             count += 1
+            lines.append(item)
+with open("missing.txt", "w") as f:
+    f.writelines(lines)
 print(count)
