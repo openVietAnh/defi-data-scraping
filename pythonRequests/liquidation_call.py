eae119ab3ce82ea37dd1d42646690fa82ee52159
openVietAnh <vietanhtran.uet@gmail.com>
2024-08-02 15:54:22 +0700

    Update python requests

--- pythonRequests/liquidation_call.py
+++ pythonRequests/liquidation_call.py
@@ -2,19 +2,21 @@
 """
     Get all liquidation call transactions from AAVE V2 subgraph
 """
-import csv
 import requests
+import csv
+import os
 
 keys, transactions = None, []
-current_time = 1654016400
+current_time = 1659286800
+START_TIME = 1654016400
 last_transactions = set()
 
-FIRST_QUERY_PART = """
-{
-        liquidationCalls(where: {timestamp_lte: 
-"""
-SECOND_QUERY_PART = """
-}, first: 1000, orderBy: timestamp, orderDirection: desc) {
+API_KEY = os.getenv("API_KEY")
+
+while True:
+    query = """
+    {
+        liquidationCalls(where: {timestamp_lte: """ + str(current_time) + """, timestamp_gt: 1654016400}, first: 1000, orderBy: timestamp, orderDirection: desc) {
             id
             pool {
                 id
@@ -34,25 +36,26 @@ SECOND_QUERY_PART = """
             timestamp
         }
     }
-"""
-
-while True:
-    query = FIRST_QUERY_PART + str(current_time) + SECOND_QUERY_PART
+    """
     response = requests.post('https://api.thegraph.com/subgraphs/name/aave/protocol-v2'
-                             '',
-                             json={'query': query})
+                                '',
+                                json={'query': query})
     if response.status_code != 200:
         print("Problem reading from timestamp", current_time, ":", response.status_code)
         continue
+    
     try:
         data = response.json()["data"]["liquidationCalls"]
-    except (KeyError, AttributeError):
+    except Exception:
         print("Error at timestamp", current_time)
         continue
+
     if len(data) == 0:
         break
+    
     if keys is None:
         keys = data[0].keys()
+
     index = 0
     try:
         while data[index]["id"] in last_transactions:
@@ -60,12 +63,16 @@ while True:
     except IndexError:
         current_time -= 1
         continue
+
+    print(len(data) - index, "transactions found at timestamp", current_time)
+
     for transaction in data[index:]:
         transaction["user"] = transaction["user"]["id"]
         transaction["pool"] = transaction["pool"]["id"]
         transaction["collateralReserve"] = transaction["collateralReserve"]["symbol"]
         transaction["principalReserve"] = transaction["principalReserve"]["symbol"]
         transactions.append(transaction)
+
     current_time = int(data[-1]["timestamp"])
     index = -1
     last_transactions = set()
@@ -77,4 +84,3 @@ with open('liquidationCall.csv', 'w', newline='') as output_file:
     DICT_WRITER = csv.DictWriter(output_file, keys)
     DICT_WRITER.writeheader()
     DICT_WRITER.writerows(transactions)
-        
